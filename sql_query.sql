d6387e4dfce786d1b9f4fd1d718827956f95e51c
Devi Smita <devismitaraj@gmail.com>
2025-10-04 11:57:05 +0530

    Update sql_query.sql

--- sql_query.sql
+++ sql_query.sql
@@ -1,8 +1,6 @@
-----*Inventory Management System*----
+**Inventory Management System**
   
-
-
--- Question-1: Checking Product Stock Levels
+-- Question-1: Checking Product Stock Levels.
 ```sql
 select p.product_name, i.quantity from products as p join inventory as i on p.product_id=i.product_id
 ```
@@ -17,11 +15,12 @@ select p.product_name, i.quantity from products as p join inventory as i on p.pr
 
   
 -- Question-2: Decrease the quantity of the product with product_id 2 (desks) by 3 units after a sale.
-
--- Update Stock After a Sale
+-- Update Stock After a Sale.
+```sql
 update inventory 
 set quantity = quantity-3
 where product_id= 2;
+```
 
 -- Results:
 -- inventory_id|product_id|quantity|supplier_id|last_updated|
@@ -32,10 +31,12 @@ where product_id= 2;
 
 
 
--- Question-3: Increase the quantity of the product with product_id 1 (laptops) by 10 units after a purchase
+-- Question-3: Increase the quantity of the product with product_id 1 (laptops) by 10 units after a purchase.
+```sql
 update inventory
 set quantity = quantity+10
 where product_id = 1;
+```
 
 -- Results:
 -- inventory_id|product_id|quantity|supplier_id|last_updated|
@@ -46,11 +47,13 @@ where product_id = 1;
 
 
 
--- Question-4: View Transaction History for the product_name Laptop
+-- Question-4: View Transaction History for the product_name Laptop.
+```sql
 select p.product_name, t.transaction_type, t.transaction_date, t.quantity 
 from products as p join transactions as t 
 on p.product_id=t.product_id
 where p.product_id =1;
+```
 
 -- Results:
 -- product_name|transaction_type|transaction_date|quantity|
@@ -61,8 +64,11 @@ where p.product_id =1;
 
 
 -- Question-5: Identify products that have stock levels below a certain threshold, such as 5 units.
+```sql
 select p.product_name, i.quantity from products as p join inventory as i on p.product_id=i.product_id
 where i.quantity<5
+```
+  
 -- Results:
 -- product_name|quantity|
 -- ------------+--------+
@@ -70,13 +76,15 @@ where i.quantity<5
 
 
   
--- Question-6: Generate a report showing the total number of products sold in july 2025:
+-- Question-6: Generate a report showing the total number of products sold in july 2025.
+```sql
 select p.product_name, sum(t.quantity) as total_quantity_sold
 from products as p join transactions as t 
 on p.product_id=t.product_id
 where t.transaction_type ='sale' and t.transaction_date between '2025-07-01' and '2025-07-31'
 group by p.product_name
-
+```
+  
 -- Results:
 -- product_name|total_quantity_sold|
 -- ------------+-------------------+
@@ -85,7 +93,8 @@ group by p.product_name
 
   
 -- Question-7: Insert a new product 'Monitor' into the Products table with category 'Electronics' and price 150.00 and 
--- Insert an initial stock quantity of 20 for the new product in the Inventory table
+-- Insert an initial stock quantity of 20 for the new product in the Inventory table.
+```sql
 insert into products (product_name, category, price)
 values ('Monitor', 'Electronics', 150.00);
 
@@ -93,6 +102,8 @@ insert into inventory (product_id, quantity)
 values ((select product_id from products where product_name = 'Monitor'),20);
 
 select * from inventory
+select * from products
+```
 -- Results:
 -- inventory_id|product_id|quantity|supplier_id|last_updated|
 -- ------------+----------+--------+-----------+------------+
@@ -101,8 +112,6 @@ select * from inventory
 --            3|       	 3|   	100|	        2|	2025-07-13|
 --            4|         4|      20|       NULL|	2025-07-13|
   
-select * from products
--- Results:
 -- product_id|product_name|price   |category    |
 -- ----------+------------+--------+-----------+
 --          1|Laptop      | 1200.00|Electronics|
@@ -111,7 +120,8 @@ select * from products
 --          4|Monitor     |  150.00|Electronics|
 
   
--- Question-8: Delete product_id = 3 from the inventory table and from the products table
+-- Question-8: Delete product_id = 3 from the inventory table and from the products table.
+```sql
 delete from inventory
 where product_id = 3
 
@@ -129,7 +139,8 @@ delete from products
 where product_id = 3
 
 select * from inventory
-
+select * from products
+```
 -- Results:
 -- inventory_id|product_id|quantity|supplier_id|last_updated|
 -- ------------+----------+--------+-----------+------------+
@@ -137,8 +148,6 @@ select * from inventory
 --            2|	       2|	     17|	        2|	2025-07-13|
 --            4|       	 4|   	 20|	     NULL|	2025-07-13|
 
-select * from products
--- Results:
 -- product_id|product_name|price   |category    |
 -- ----------+------------+--------+-----------+
 --          1|Laptop      | 1200.00|Electronics|
@@ -147,9 +156,11 @@ select * from products
 
 
   
--- Question-9: calculate total value for each product
+-- Question-9: calculate total value for each product.
+```sql
 select p.product_name, i.quantity, p.price, (i.quantity * p.price) as total_value
 from products as p join inventory as i on p.product_id=i.product_id;
+```
 
 -- Results:
 -- product_name|quantity|price   |total_value|
@@ -160,11 +171,13 @@ from products as p join inventory as i on p.product_id=i.product_id;
 
 
 
--- Question-10: Show Products with Transactions to include all products, even those with no matching transactions
+-- Question-10: Show Products with Transactions to include all products, even those with no matching transactions.
+```sql
 select p.product_name from products as p left join transactions as t 
 on p.product_id = t.product_id and t.transaction_type = 'sale'
 group by p.product_name
 having MAX(t.transaction_date) is null or MAX(t.transaction_date) < '2025-07-01';
+```
 
 -- Results:
 -- product_name|
@@ -174,10 +187,11 @@ having MAX(t.transaction_date) is null or MAX(t.transaction_date) < '2025-07-01'
 
 
 
--- Question-11:List Products with No Stock
+-- Question-11:List Products with No Stock.
+```sql
 select p.product_name from Inventory as i join products as p on i.product_id = p.product_id
 WHERE i.quantity = 0;
-
+```
 -- Results:
 -- product_name|
 -- ------------+
@@ -185,10 +199,11 @@ WHERE i.quantity = 0;
 
 
 
--- Question-12: View Stock Levels for Multiple Products
+-- Question-12: View Stock Levels for Multiple Products.
+```sql
 select p.product_name, i.quantity from products as p join inventory as i on p.product_id=i.product_id
 where p.product_id In (1,2)
-
+```
 -- Results:
 -- product_name|quantity|
 -- ------------+--------+
@@ -197,11 +212,13 @@ where p.product_id In (1,2)
 
 
   
--- Question-13: List All Products Sold Today
+-- Question-13: List All Products Sold Today.
+```sql
 select p.product_name, t.quantity
 from Transactions as t
 join Products p on t.product_id = p.product_id
 where t.transaction_type = 'sale' and t.transaction_date = cast(getdate() as date);
+```
 
 -- Results:
 -- product_name|quantity|
@@ -210,14 +227,16 @@ where t.transaction_type = 'sale' and t.transaction_date = cast(getdate() as dat
 
 
 
--- Question-14: Find the Most Sold Product
+-- Question-14: Find the Most Sold Product.
+```sql
 select top 1 p.product_name, SUM(t.quantity) as total_sold
 from products as p join transcations as t on t.product_id = p.product_id
 where t.transaction_type = 'sale'
 and t.transaction_date between '2025-07-01' and '2025-07-31'
 group by p.product_name
 order by total_sold desc
-
+```
+  
 -- Results:
 -- product_name|quantity|
 -- ------------+--------+
@@ -225,12 +244,14 @@ order by total_sold desc
 
 
 
--- Question-15: Calculate Total Revenue for a Given Period
+-- Question-15: Calculate Total Revenue for a Given Period.
+```sql
 select sum(t.quantity * p.price) as total_revenue
 from products as p join transactions as t
 on p.product_id = t.product_id
 WHERE t.transaction_type = 'sale'
 AND t.transaction_date BETWEEN '2025-07-01' AND '2025-07-31';
+```
 
 -- Results:
 -- total_revenue|
